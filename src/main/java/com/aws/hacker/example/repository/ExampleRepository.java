273baae6bfedb74564cb6556d21316ff890d0909
RyuTaeUung <aser77992@gmail.com>
2024-08-21 01:35:30 +0900

    feat : example 삭제

--- src/main/java/com/aws/hacker/example/repository/ExampleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aws.hacker.example.repository;
-
-import com.aws.hacker.example.entity.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleRepository extends JpaRepository<Example, Long> {
-}
