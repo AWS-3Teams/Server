3671f778e4e8f8cfeb0ae09037581c23318165e0
hyeseong <hen715@naver.com>
2024-08-21 01:42:33 +0900

    refactor:종속성 제거

--- src/test/java/com/aws/hacker/AwsApplicationTests.java
+++ src/test/java/com/aws/hacker/AwsApplicationTests.java
@@ -3,10 +3,10 @@ package com.aws.hacker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AwsApplicationTests {
 
-    @Test
+   // @Test
     void contextLoads() {
     }
 
