495c6555bf452828456b02504490a28f14881fbf
RyuTaeUung <aser77992@gmail.com>
2024-08-21 03:02:27 +0900

    feat : Tensorflow 응답

--- src/main/java/com/aws/hacker/multipart/controller/MultipartController.java
+++ src/main/java/com/aws/hacker/multipart/controller/MultipartController.java
@@ -2,10 +2,12 @@ package com.aws.hacker.multipart.controller;
 
 import com.aws.hacker.multipart.service.TensorFlowService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -19,7 +21,7 @@ public class MultipartController {
     private final TensorFlowService tensorFlowService;
 
     @RequestMapping("/upload")
-    public float[] uploadImg(@RequestPart List<MultipartFile> files) {
+    public ResponseEntity<float[]> uploadImg(@RequestPart List<MultipartFile> files) {
         MultipartFile uploadedFile = files.get(0);
         try {
             // 멀티파트 파일을 이미지로 로드합니다.
@@ -27,7 +29,9 @@ public class MultipartController {
             // 이미지를 TensorFlow 입력 형식에 맞게 전처리합니다.
             float[] imageData = preprocessImage(image);
             // TensorFlow 모델에 입력하여 결과를 얻습니다.
-            return tensorFlowService.predict(imageData);
+            float[] predictions = tensorFlowService.predict(imageData);
+            // 예측 결과를 응답으로 전송합니다.
+            return ResponseEntity.ok(predictions);
         } catch (IOException e) {
             throw new RuntimeException("이미지 파일을 처리하는 중 오류가 발생했습니다.", e);
         }
